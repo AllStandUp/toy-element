6b0770ef1aafaa202cf13f5f4e62508583a07583
清欢 <2633317981@qq.com>
2024-07-08 08:55:45 +0800

    :tada: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,12 @@
+<script lang='ts' setup>
+    defineOptions({
+        name: 'ErButton'
+    })
+</script>
+<template>
+    <button style="color:red;">test button</button>
+  </template>
+
+<style lang = "scss" scoped>
+
+</style>
